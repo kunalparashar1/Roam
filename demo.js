ae17fa0c73bc9632f516950a7121160a0f7dd3d2
= <=>
2020-01-18 18:36:36 +0530

    some changes

--- demo.js
+++ demo.js
@@ -1,4 +1,9 @@
-unction moveMapToBerlin(map){
+/**
+ * Moves the map to display over Berlin
+ *
+ * @param  {H.Map} map      A HERE Map instance within the application
+ */
+function moveMapToBerlin(map){
   map.setCenter({lat:52.5159, lng:13.3777});
   map.setZoom(14);
 }
@@ -10,19 +15,7 @@ unction moveMapToBerlin(map){
 //Step 1: initialize communication with the platform
 // In your own code, replace variable window.apikey with your own apikey
 var platform = new H.service.Platform({
-  apikey: window.unction moveMapToBerlin(map){
-  map.setCenter({lat:52.5159, lng:13.3777});
-  map.setZoom(14);
-}
-
-/**
- * Boilerplate map initialization code starts below:
- */
-
-//Step 1: initialize communication with the platform
-// In your own code, replace variable window.apikey with your own apikey
-var platform = new H.service.Platform({
-  apikey: window.apikey
+  apikey: window.QaP7WjVJNH6ku6zKxh2wsjmmCPo-LNWQyW0--ZXlinU
 });
 var defaultLayers = platform.createDefaultLayers();
 
@@ -48,30 +41,3 @@ var ui = H.ui.UI.createDefault(map, defaultLayers);
 window.onload = function () {
   moveMapToBerlin(map);
 }
-© 2020 GitHub, Inc.
-});
-var defaultLayers = platform.createDefaultLayers();
-
-//Step 2: initialize a map - this map is centered over Europe
-var map = new H.Map(document.getElementById('map'),
-  defaultLayers.vector.normal.map,{
-  center: {lat:50, lng:5},
-  zoom: 4,
-  pixelRatio: window.devicePixelRatio || 1
-});
-// add a resize listener to make sure that the map occupies the whole container
-window.addEventListener('resize', () => map.getViewPort().resize());
-
-//Step 3: make the map interactive
-// MapEvents enables the event system
-// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-
-// Create the default UI components
-var ui = H.ui.UI.createDefault(map, defaultLayers);
-
-// Now use the map as required...
-window.onload = function () {
-  moveMapToBerlin(map);
-}
-© 2020 GitHub, Inc.
